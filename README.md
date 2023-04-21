79e52f632bfbf9293833ff2c1edd26941a9d9037
jbroom86 <jbroom86@gmail.com>
2023-04-21 12:47:24 -0700

    setup file structure for frontend and backend, installed react and tailwindscss.

--- README.md
+++
@@ -1,1 +0,0 @@
-# Services-MERN-Project
---
+++ frontend/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline bg-red-500">Test test test</h1>
+    </>
+  )
+}
+
+export default App
