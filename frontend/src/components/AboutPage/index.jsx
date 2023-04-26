d40b204da90c38324a18b0992bda5fa09cbc66df
jbroom86 <jbroom86@gmail.com>
2023-04-26 09:41:34 -0700

    added content to the about me section

--- frontend/src/components/AboutPage/index.jsx
+++ frontend/src/components/AboutPage/index.jsx
@@ -1,5 +1,37 @@
 function AboutPage() {
-    return <h1>About Page will go here!</h1>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-4">About Aireonna</h1>
+
+      <div className="max-w-3xl w-full">
+        <p className="text-lg leading-7 mb-4 p-6 border border-gray-300 rounded-md shadow-md">
+          Aireonna is a driven and passionate individual who is currently
+          pursuing a career in the beauty industry. She is enrolled in
+          cosmetology school with the goal of becoming an esthetician,
+          specializing in skincare and beauty treatments. Aireonna has always
+          been interested in the beauty industry and enjoys exploring the latest
+          trends and techniques in skincare.
+        </p>
+
+        <p className="text-lg leading-7 mb-4 p-6 border border-gray-300 rounded-md shadow-md">
+          Throughout her education, Aireonna has demonstrated a strong work
+          ethic and dedication to her studies. She is highly detail-oriented and
+          strives for perfection in all aspects of her work. In addition to her
+          academic achievements, Aireonna has gained practical experience by
+          working in various salons and spas, honing her skills and building her
+          clientele.
+        </p>
+
+        <p className="text-lg leading-7 mb-4 p-6 border border-gray-300 rounded-md shadow-md">
+          Aireonna's ultimate goal is to open her own spa, where she can create
+          a relaxing and welcoming environment for her clients while providing
+          top-notch beauty services. With her passion, dedication, and drive,
+          Aireonna is well on her way to achieving her dreams and making a name
+          for herself in the beauty industry.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
