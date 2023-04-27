cf6c927de2e560c4a7f6fd0bc3881d8da26f6579
jbroom86 <jbroom86@gmail.com>
2023-04-26 17:17:35 -0700

    added homepage content

--- frontend/src/components/Homepage/index.jsx
+++ frontend/src/components/Homepage/index.jsx
@@ -1,16 +1,21 @@
-import { useState, useEffect } from "react"
-import { getServices } from "../../../utils/backend"
+import { useState, useEffect } from "react";
+import { getServices } from "../../../utils/backend";
+import { Link } from "react-router-dom";
 
 function HomePage() {
-    
-    
-
-
-    return (
-        <>
-            <h1>Homepage information will go here.</h1>
-        </>
-    )
+  return (
+    <>
+      <div class="flex justify-center items-center mt-8">
+        <Link to={"/services"}>
+          <img
+            src="..\src\assets\photos\IMG_6922.jpg"
+            
+            class="rounded-full w-3/4 mx-auto hover:shadow-2xl focus:shadow-lg hover:rotate-45 transform transition-all duration-500 ease-in-out"
+          />
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
