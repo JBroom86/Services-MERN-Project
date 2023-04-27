8d2c27612d708498c769e5a5e4eb90058d35ffe0
jbroom86 <jbroom86@gmail.com>
2023-04-27 13:33:10 -0700

    reformatted app/index.jsx

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -1,53 +1,36 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Header from '../Header'
-import Footer from '../Footer'
-import HomePage from '../Homepage'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "../Header";
+import Footer from "../Footer";
+import HomePage from "../Homepage";
 
-import AboutPage from '../AboutPage'
-import ProductsPage from '../ProductsPage'
-import ServicesPage from '../ServicesPage'
-import ServiceDetailsPage from '../ServiceDetailsPage'
-import CreateServicePage from '../CreateServicePage'
-import AuthFormPage from '../AuthFormPage'
+import AboutPage from "../AboutPage";
+import ProductsPage from "../ProductsPage";
+import ServicesPage from "../ServicesPage";
+import ServiceDetailsPage from "../ServiceDetailsPage";
+import CreateServicePage from "../CreateServicePage";
+import AuthFormPage from "../AuthFormPage";
 
-
-import './styles.css'
-
-
-
-
+import "./styles.css";
 
 function App() {
-  
-
   return (
-
     <>
       <Header />
-    
 
-      
-      
-        <Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/services/:serviceId" element={<ServiceDetailsPage />} />
+        <Route path="/services/create" element={<CreateServicePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/auth/:formType" element={<AuthFormPage />} />
+      </Routes>
 
-            <Route path="/" element={<HomePage />} />   
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/services/:serviceId" element={<ServiceDetailsPage />} />
-            <Route path='/services/create' element= {<CreateServicePage/>} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/auth/:formType" element={<AuthFormPage />} />
-            
-
-        </Routes>
-      
       <Footer />
-      
     </>
-  )
-
-  
+  );
 }
 
-export default App
+export default App;
