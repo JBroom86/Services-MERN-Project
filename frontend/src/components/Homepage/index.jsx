c3f79439f3a8e46bfd8936c920597678113f7a6b
jbroom86 <jbroom86@gmail.com>
2023-04-26 19:59:18 -0700

    correct classname

--- frontend/src/components/Homepage/index.jsx
+++ frontend/src/components/Homepage/index.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <>
-      <div class="flex justify-center items-center mt-8">
+      <div className="flex justify-center items-center mt-8">
         <Link to={"/services"}>
           <img
             src="..\src\assets\photos\IMG_6922.jpg"
             
-            class="rounded-full w-3/4 sm:w-3/4 mx-auto hover:shadow-2xl focus:shadow-lg hover:rotate-45 transform transition-all duration-500 ease-in-out"
+            className="rounded-full w-3/4 sm:w-3/4 mx-auto hover:shadow-2xl focus:shadow-lg hover:rotate-45 transform transition-all duration-500 ease-in-out"
           />
         </Link>
       </div>
