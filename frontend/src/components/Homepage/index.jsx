e71413d92eded7c679731966933dae9f137fc31c
jbroom86 <jbroom86@gmail.com>
2023-04-28 11:04:49 -0700

    update path for homepage photo

--- frontend/src/components/Homepage/index.jsx
+++ frontend/src/components/Homepage/index.jsx
@@ -8,7 +8,7 @@ function HomePage() {
       <div className="flex justify-center items-center mt-8">
         <Link to={"/services"}>
           <img
-            src="..\src\assets\photos\IMG_6922.jpg"
+            src="https://i.imgur.com/MQeslyo.jpg"
             
             className="rounded-full w-3/4 sm:w-3/4 mx-auto hover:shadow-2xl focus:shadow-lg hover:rotate-45 transform transition-all duration-500 ease-in-out"
           />
