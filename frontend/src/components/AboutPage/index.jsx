8d2e946c1e34df743620773baea96f2a46645a3b
jbroom86 <jbroom86@gmail.com>
2023-04-28 15:05:10 -0700

    updated styling on the header and about me pages to correct about me page overlaying the header.

--- frontend/src/components/AboutPage/index.jsx
+++ frontend/src/components/AboutPage/index.jsx
@@ -1,33 +1,19 @@
 function AboutPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">About Aireonna</h1>
+    <div className="flex flex-col items-center justify-center h-screen pt-4 sm:pt-16">
+      <h1 className="text-2xl sm:text-3xl font-bold mt-8 sm:mt-12 mb-4">About Aireonna</h1>
 
-      <div className="max-w-3xl w-full">
-        <p className="text-lg leading-7 mb-4 p-6 border border-gray-300 rounded-md shadow-md">
-          Aireonna is a driven and passionate individual who is currently
-          pursuing a career in the beauty industry. She is enrolled in
-          cosmetology school with the goal of becoming an esthetician,
-          specializing in skincare and beauty treatments. Aireonna has always
-          been interested in the beauty industry and enjoys exploring the latest
-          trends and techniques in skincare.
+      <div className="w-full px-4 sm:px-6 lg:px-8">
+        <p className="text-lg leading-7 mb-4 p-4 sm:p-6 border border-gray-300 rounded-md shadow-md">
+          Aireonna is a driven and passionate individual who is currently pursuing a career in the beauty industry. She is enrolled in cosmetology school with the goal of becoming an esthetician, specializing in skincare and beauty treatments. Aireonna has always been interested in the beauty industry and enjoys exploring the latest trends and techniques in skincare.
         </p>
 
-        <p className="text-lg leading-7 mb-4 p-6 border border-gray-300 rounded-md shadow-md">
-          Throughout her education, Aireonna has demonstrated a strong work
-          ethic and dedication to her studies. She is highly detail-oriented and
-          strives for perfection in all aspects of her work. In addition to her
-          academic achievements, Aireonna has gained practical experience by
-          working in various salons and spas, honing her skills and building her
-          clientele.
+        <p className="text-lg leading-7 mb-4 p-4 sm:p-6 border border-gray-300 rounded-md shadow-md">
+          Throughout her education, Aireonna has demonstrated a strong work ethic and dedication to her studies. She is highly detail-oriented and strives for perfection in all aspects of her work. In addition to her academic achievements, Aireonna has gained practical experience by working in various salons and spas, honing her skills and building her clientele.
         </p>
 
-        <p className="text-lg leading-7 mb-4 p-6 border border-gray-300 rounded-md shadow-md">
-          Aireonna's ultimate goal is to open her own spa, where she can create
-          a relaxing and welcoming environment for her clients while providing
-          top-notch beauty services. With her passion, dedication, and drive,
-          Aireonna is well on her way to achieving her dreams and making a name
-          for herself in the beauty industry.
+        <p className="text-lg leading-7 mb-4 p-4 sm:p-6 border border-gray-300 rounded-md shadow-md">
+          Aireonna's ultimate goal is to open her own spa, where she can create a relaxing and welcoming environment for her clients while providing top-notch beauty services. With her passion, dedication, and drive, Aireonna is well on her way to achieving her dreams and making a name for herself in the beauty industry.
         </p>
       </div>
     </div>
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -2,32 +2,32 @@ import { Route, Routes, Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="p-4 flex flex-col sm:flex-row justify-between items-center">
+    <header className="p-4 flex flex-col sm:flex-row sm:justify-between sm:items-center">
       <Link to="/" className="flex items-center gap-1">
         <img
           src="https://static.thenounproject.com/png/1335654-200.png"
           alt=""
           className="w-20 h-20"
         />
-        <span>
+        <span className="text-sm">
           <strong>Absolute Skin</strong> by Aireonna
         </span>
       </Link>
-      <div className="flex items-center mt-4 sm:mt-0  border border-brown-300 rounded-full p-2 shadow-md shadow-gray-300">
+      <div className="flex items-center mt-4 sm:mt-0 border border-brown-300 rounded-full p-2 shadow-md shadow-gray-300">
         <div className="p-4">
-          <Link to="/services">Services</Link>
+          <Link to="/services" className="text-sm">Services</Link>
         </div>
         <div className="p-4">
-          <Link to="/products">Products</Link>
+          <Link to="/products" className="text-sm">Products</Link>
         </div>
         <div className="p-4">
-          <Link to="/about">About Me</Link>
+          <Link to="/about" className="text-sm">About Me</Link>
         </div>
         <div className="p-4">
-          <Link to={"/auth/login"}>Log In</Link>
+          <Link to={"/auth/login"} className="text-sm">Log In</Link>
         </div>
         {/* <div className="p-4">
-          <Link to="/auth/signup">Sign Up</Link>
+          <Link to="/auth/signup" className="text-sm">Sign Up</Link>
         </div> */}
       </div>
     </header>
