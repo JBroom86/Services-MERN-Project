9847f8906a015d2612ee948ec90951d320ad3fb6
jbroom86 <jbroom86@gmail.com>
2023-04-28 10:39:39 -0700

    updated backend.js

--- frontend/utils/backend.js
+++ frontend/utils/backend.js
@@ -5,37 +5,37 @@ const authHeader = { headers: { 'Authorization': localStorage.getItem('userToken
 
 // the await needs to be before the axios request because it is an async function
 export async function getServices() {
-    const {data} = await axios.get('http://localhost:3000/services')
+    const {data} = await axios.get('/services')
     return data
 }
 
 export async function getServicesId(id) {
-    const {data} = await axios.get('http://localhost:3000/services/' + id) 
+    const {data} = await axios.get('/services/' + id) 
     return data
 }
 
 export async function deleteService(id) {
-    axios.delete(`http://localhost:3000/services/${id}`)
+    axios.delete(`/services/${id}`)
     
 } 
 
 export async function createService(serviceData) {
-  const { data } = await axios.post("http://localhost:3000/services", serviceData);
+  const { data } = await axios.post("/services", serviceData);
   return data;
 }
 
 export async function updateService(id, updatedServiceData) {
-    const { data } = await axios.put(`http://localhost:3000/services/${id}`, updatedServiceData);
+    const { data } = await axios.put(`/services/${id}`, updatedServiceData);
     return data;
   }
 
   export async function signUp(user) {
-    const { data } = await axios.post('http://localhost:3000/users/signup', user)
+    const { data } = await axios.post('/users/signup', user)
     return data
 }
 
 export async function logIn(user) {
-    const { data } = await axios.post('http://localhost:3000/users/login', user)
+    const { data } = await axios.post('/users/login', user)
     return data
 }
 
