292bfde01df31ce623b86c0e9192af0e92664bf7
jbroom86 <jbroom86@gmail.com>
2023-04-28 10:55:38 -0700

    updated api.js

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export async function getData(url) {
-    const response = await axios.get(url)
-    const data = await response.data
+    const { data } = await axios.get(url)
     return data
 }
