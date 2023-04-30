440cf3217f6342a1605bb610e5ac52486ebba34a
jbroom86 <jbroom86@gmail.com>
2023-04-30 13:50:05 -0700

    updated products photos.

--- frontend/src/components/ProductsPage/index.jsx
+++ frontend/src/components/ProductsPage/index.jsx
@@ -1,5 +1,42 @@
 function ProductsPage() {
-    return <h1>Products Page will go here!</h1>
+    return (
+        <>
+
+            <div>
+                <h3>Cleanser</h3>
+                <img src="https://www.sephora.com/productimages/sku/s2002103-main-zoom.jpg?imwidth=315"/>
+                <p>A pH-balanced, foaming gel cleanser that gently removes impurities without disrupting your skin’s natural moisture balance.</p>
+            </div>
+            <div>
+                <h3>Exfoliate</h3>
+                <img src="https://cdn.shopify.com/s/files/1/0741/4585/7823/products/Dermalogica--Daily-Microfoliant-Dermalogica-1662735031.jpg?v=1681775107"/>
+                <p>An iconic exfoliating enzyme powder that activates upon contact with water, this powerful, yet gentle powder face scrub helps to even skin tone, clear blemishes and reduce the appearance of pores.</p>
+            </div>
+            <div>
+                <h3>Treat</h3>
+                <img src="https://www.sephora.com/productimages/product/p481697-av-06-zoom.jpg"/>
+                <p>A hyaluronic acid serum that hydrates and helps prevent future dehydration</p>
+            </div>
+            <div>
+                <h3>Moisturizer</h3>
+                <img src="https://cdn.shopify.com/s/files/1/0209/5256/products/523_844afa8c-b499-4404-bb61-ef11f7e62453.jpeg?v=1381245014"/>
+                <p>This lightweight, oil-free moisturizer absorbs without a trace, creating an invisible barrier against moisture loss. Silk proteins join extracts of burdock, sage and cucumber to calm and hydrate the skin.</p>
+            </div>
+            <div>
+                <h3>Eye Cream</h3>
+                <img src="https://cdn.mall.adeptmind.ai/https%3A%2F%2Fwww.sephora.com%2Fproductimages%2Fsku%2Fs2002301-main-zoom.jpg_640x.jpg"/>
+                <p>A visibly firming eye cream formulated with vitamins A, C, and E to support natural collagen production, smooth the look of fine lines, and protect against pollution.</p>
+            </div>
+            <div>
+                <h3>Protect</h3>
+                <img src="https://cdnimg.lanaika.com/4618-large_default3/protection-solaire-protection50-sport-spf50-dermalogica.jpg"/>
+                <p>A multitasking SPF 40 mineral sunscreen with a blurring effect that supports healthy-looking pores and provides a hint of tint for an instantly radiant complexion.</p>
+            </div>
+
+
+
+        </>
+    )
 }
 
 export default ProductsPage
